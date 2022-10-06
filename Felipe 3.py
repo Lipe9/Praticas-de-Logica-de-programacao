8a4868e0e9b9a2a9500d7126b00d952a487eddb4
fellipe157 <115155968+fellipe157@users.noreply.github.com>
2022-10-06 08:39:25 -0300

    Add files via upload

---
+++ Felipe 3.py
@@ -0,0 +1,13 @@
+ 
+ano = int(input("Digite ano do seu nascimento:"))
+ano_atual = int(input("Digite ano atual:"))
+
+print(f'''Sua idade é''')
+print(ano_atual - ano)
+print(f'''Sua idade em 2050 será:''')
+print(2050 - ano)
+
+if ano >= 100
+print("Você é um centenario")
+else
+print("Vocé não é um centenario")
